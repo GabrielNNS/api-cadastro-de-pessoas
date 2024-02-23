c08c283128250a9f1ec0a6b760c83ad36d55407a
GabrielNS <gabriel.nascimento33@hotmail.com>
2024-02-23 13:18:33 -0300

    update DTO

--- cadastro_pessoas/src/main/java/br/tec/db/cadastro_pessoas/dto/PessoaUpdateDTO.java
+++ cadastro_pessoas/src/main/java/br/tec/db/cadastro_pessoas/dto/PessoaUpdateDTO.java
@@ -1,13 +1,8 @@
 package br.tec.db.cadastro_pessoas.dto;
 
-import br.tec.db.cadastro_pessoas.model.Endereco;
 import jakarta.validation.constraints.NotBlank;
-
-import java.util.List;
 
 public record PessoaUpdateDTO(
         @NotBlank
-        String nome,
-
-        List<Endereco> endereco) {
+        String nome) {
 }
